4a1829ba455242182a864b46d6353c0ddfac0936
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-11-29 18:04:16 +0100

    feat: added gatsby-plugin-typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
