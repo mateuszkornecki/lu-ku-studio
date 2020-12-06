c957d1db7c543bc0da1f885becb719876b2fb6be
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-12-06 21:22:38 +0100

    feat: added font sources

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,22 +1,13 @@
 import React, { ReactNode } from "react"
 import "./layout.css"
+import "fontsource-roboto-condensed"
 
 type LayoutProps = {
   children: ReactNode
 }
 
 const Layout = (props: LayoutProps) => {
-  return (
-    <div
-      className="bg-blue-50"
-      style={{
-        padding: "3rem",
-        height: "100vh",
-      }}
-    >
-      {props.children}
-    </div>
-  )
+  return <div className="p-8">{props.children}</div>
 }
 
 export default Layout
