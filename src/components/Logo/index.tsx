b45fd4486d6e72423f516938241ccec73c01882d
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-12-06 21:22:00 +0100

    feat: added logo styles

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import "./styles.css"
 
 const Logo = () => {
-  return <h1 style={{ margin: 0 }}>L.U/KO STUDIO</h1>
+  return <h1 className="logo m-0 mb-8 text-4xl font-bold">LU / KO STUDIO</h1>
 }
 
 export default Logo
