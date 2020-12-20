9b0606da3247c116624bd588456d0514cbc6e2ad
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-12-20 18:49:10 +0100

    feat: added active state handling to navigation item

--- src/components/NavigationItem/index.tsx
+++ src/components/NavigationItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactText } from "react"
+import React, { ReactText, useState } from "react"
 import { Link } from "gatsby"
 
 type Index = {
@@ -7,11 +7,25 @@ type Index = {
 }
 
 const NavigationItem = (props: Index) => {
+  const [active, setActive] = useState(false)
   const { children, redirectTo } = props
 
   return (
-    <Link to={redirectTo}>
-      <li className="m-0 text-xl duration-500 ease-in-out text-black-400 transform hover:translate-x-8">
+    <Link
+      to={redirectTo}
+      activeClassName={"transform translate-x-8"}
+      getProps={({ isCurrent }) => {
+        if (isCurrent) {
+          setActive(true)
+        }
+        return {}
+      }}
+    >
+      <li
+        className={`${
+          active ? "transform translate-x-8" : ""
+        } m-0 text-xl duration-500 ease-in-out text-black-400 hover:transform hover:translate-x-8`}
+      >
         / {String(children).toUpperCase()}
       </li>
     </Link>
