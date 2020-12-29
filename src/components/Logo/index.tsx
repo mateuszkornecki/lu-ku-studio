eaa93f401af068feda6fc98fcddad694fbefef2c
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-12-29 20:54:19 +0100

    feat: added home link to the Logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,23 @@
 import React from "react"
 import "./styles.css"
+import { Link } from "gatsby"
 
-const Logo = () => {
-  return <h1 className="logo m-0 mb-8 text-4xl font-bold">LU / KO STUDIO</h1>
+type LogoProps = {
+  setActive: (active: string) => void
+}
+
+const Logo = (props: LogoProps) => {
+  const { setActive } = props
+  return (
+    <Link
+      to={"/"}
+      onClick={() => {
+        setActive("/")
+      }}
+    >
+      <h1 className="logo m-0 mb-8 text-4xl font-bold">LU / KO STUDIO</h1>
+    </Link>
+  )
 }
 
 export default Logo
