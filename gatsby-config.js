4ac761e9f56d904dd418c17451d3a2cbfa4018db
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-12-29 22:26:36 +0100

    deps: deleted gatsby-plugin-preload-fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-layout`,
-    `gatsby-plugin-preload-fonts`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
