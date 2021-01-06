3b606d520680f2cd878c05f3ae506ab97dc639e8
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2021-01-06 11:07:02 +0100

    feat: added mobile layout for projectTemplate

--- src/templates/projectTemplate.tsx
+++ src/templates/projectTemplate.tsx
@@ -1,46 +1,84 @@
 import React from "react"
+
 import Img from "gatsby-image"
 import { graphql } from "gatsby"
 
 import Logo from "../components/Logo"
 import NavigationBack from "../components/NavigationBack"
 
+import useWidth from "../hooks/useWidth"
+
 export default function Template({ data }) {
   const { markdownRemark, allFile } = data
   const { frontmatter } = markdownRemark
+  const { isMobile } = useWidth()
 
-  return (
-    <main className="grid grid-cols-2">
+  const projectDescription = (
+    <React.Fragment>
+      <p className="mb-4 text-xs">{frontmatter.description}</p>
+      <span className="flex">
+        <p className="pr-4 text-xs">Autor:</p>
+        <p className="text-xs">{frontmatter.author}</p>
+      </span>
+      <span className="flex">
+        <p className="pr-4 text-xs">Lokalizacja:</p>
+        <p className="text-xs">{frontmatter.location}</p>
+      </span>
+      <p className="text-xs">{frontmatter.footer}</p>
+    </React.Fragment>
+  )
+
+  const projectGallerySection = (
+    <section className={isMobile ? "" : `h-screen overflow-y-scroll`}>
+      {allFile.edges.map(edge => {
+        return (
+          <div className={isMobile ? "pb-8" : ""}>
+            <Img fluid={edge.node.childImageSharp.fluid} />
+          </div>
+        )
+      })}
+    </section>
+  )
+
+  const desktopLayout = (
+    <main className="grid md:grid-cols-2">
       <section className="flex flex-col justify-between p-8 h-screen">
         <header>
           <Logo />
           <NavigationBack navigateTo="/architektura" />
         </header>
-        <article className="flex max-w-65p text-justify	">
+        <article className="flex text-justify	">
           <div>
             <h2 className="uppercase text-2xl font-bold mb-4">
               {frontmatter.title}
             </h2>
-            <p className="mb-4 text-xs">{frontmatter.description}</p>
-            <span className="flex">
-              <p className="pr-4 text-xs">Autor:</p>
-              <p className="text-xs">{frontmatter.author}</p>
-            </span>
-            <span className="flex">
-              <p className="pr-4 text-xs">Lokalizacja:</p>
-              <p className="text-xs">{frontmatter.location}</p>
-            </span>
-            <p className="text-xs">{frontmatter.footer}</p>
+            {projectDescription}
           </div>
         </article>
       </section>
-      <section className="h-screen overflow-y-scroll">
-        {allFile.edges.map(edge => {
-          return <Img fluid={edge.node.childImageSharp.fluid} />
-        })}
+      {projectGallerySection}
+    </main>
+  )
+
+  const mobileLayout = (
+    <main>
+      <section className="p-8">
+        <header className="pb-8">
+          <Logo />
+          <NavigationBack navigateTo="/architektura" />
+        </header>
+        <h2 className="uppercase text-2xl font-bold">{frontmatter.title}</h2>
+      </section>
+      <section>
+        <article className="">
+          {projectGallerySection}
+          <div className="p-8 pt-0">{projectDescription}</div>
+        </article>
       </section>
     </main>
   )
+
+  return isMobile ? mobileLayout : desktopLayout
 }
 export const pageQuery = graphql`
   query($slug: String!, $imagesSlug: String!) {
