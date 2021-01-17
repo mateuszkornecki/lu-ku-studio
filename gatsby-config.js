8bbcfbcea28a17fa62372ae768f0ddfc2e2b1c1c
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2021-01-17 19:22:40 +0100

    feat: added gatsby-netlify-cms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,7 @@ module.exports = {
         postCssPlugins: [require("tailwindcss")],
       },
     },
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
