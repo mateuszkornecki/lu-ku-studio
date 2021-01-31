d8444ee7fc1c11878bd65204e07031a4463a41e6
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2021-01-31 19:23:39 +0100

    feat: fixed infoPage layout on mobiles

--- src/templates/infoTemplate.tsx
+++ src/templates/infoTemplate.tsx
@@ -2,22 +2,27 @@ import React from "react"
 
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import useWidth from "../hooks/useWidth"
 
 const InfoPage = ({ data: { allFile, markdownRemark } }) => {
   const {
     frontmatter: { description, telephone, email },
   } = markdownRemark
 
+  const { isMobile } = useWidth()
+
   const profileImage = allFile.edges[0].node.childImageSharp.fluid
 
   return (
-    <main className="m-8">
-      <article className="text-justify text-xs pt-8 md:pt-0 md:h-screen md:overflow-y-scroll">
-        <Img fluid={profileImage} />
-        <section className="md:max-w-65p px-8 md:px-0">
-          <p className="pt-8 ">{description}</p>
+    <main>
+      <article className="md:p-8 text-justify text-xs pt-8 md:pt-0 md:h-screen md:overflow-y-scroll">
+        <Img className={isMobile ? "mx-8" : "mt-8"} fluid={profileImage} />
+        <section className="px-8 md:px-0 2xl:max-w-65p">
+          <p className="pt-8" style={{ flexGrow: 1, flexBasis: "" }}>
+            {description}
+          </p>
           <p className="mt-4">Kontakt E: {email} </p>
-          <p className="">T: {telephone}</p>
+          <p className={isMobile ? "pb-8" : ""}>T: {telephone}</p>
         </section>
       </article>
     </main>
