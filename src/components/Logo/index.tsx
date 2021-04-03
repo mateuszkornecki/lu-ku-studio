58a6c593514efa4d74e8864b6324b1d8a9c98451
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2021-04-03 18:17:57 +0200

    feat: updated site logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,8 +5,8 @@ import { Link } from "gatsby"
 const Logo = () => {
   return (
     <Link to={"/"}>
-      <h1 className="logo m-0 mb-8 text-2xl md:text-4xl font-bold">
-        LU / KO STUDIO
+      <h1 className="logo m-0 mb-8 text-2xl md:text-4xl font-bold text-white">
+        <span className="-ml-8 pl-8 bg-black">LU / KO STUDIO</span>
       </h1>
     </Link>
   )
