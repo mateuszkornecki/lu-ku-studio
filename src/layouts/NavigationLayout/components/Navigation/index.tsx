8aa3bd7da412e42ceef394216114977dc46bd93c
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2021-04-04 20:51:09 +0200

    feat: added toggled navigation on the mobiles

--- src/layouts/NavigationLayout/components/Navigation/index.tsx
+++ src/layouts/NavigationLayout/components/Navigation/index.tsx
@@ -2,13 +2,38 @@ import React, { ReactElement, ReactNode } from "react"
 import NavigationItem from "../NavigationItem"
 
 type NavigationProps = {
-  children: ReactNode
+  path: string
+  onClick?: () => void
 }
 
 const Navigation = (props: NavigationProps) => {
+  const { path, onClick } = props
+
   return (
     <nav>
-      <ul className="m-0">{props.children}</ul>
+      <ul className="m-0">
+        <NavigationItem
+          active={path === "/architektura/"}
+          redirectTo="/architektura"
+          onClick={onClick}
+        >
+          Architektura
+        </NavigationItem>
+        <NavigationItem
+          active={path === "/wnetrza/"}
+          redirectTo="/wnetrza"
+          onClick={onClick}
+        >
+          Wnętrza
+        </NavigationItem>
+        <NavigationItem
+          active={path === "/info/"}
+          redirectTo="/info"
+          onClick={onClick}
+        >
+          Info
+        </NavigationItem>
+      </ul>
     </nav>
   )
 }
